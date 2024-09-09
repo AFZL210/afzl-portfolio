1d312f538e323e6ea63fdb6c5c762d7bee6f5083
AFZL210 <tricktomade@gmail.com>
2024-09-09 19:47:36 +0530

    add featured projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,41 @@
 import Image from "next/image";
+import Link from "next/link";
+import { GitHubLogoIcon, LinkedInLogoIcon, TwitterLogoIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+import data from '@/data/data.json';
+import { Button } from "@/components/ui/button";
+import FeaturedProject from "@/components/ui/FeaturedProject";
 
 export default function Home() {
   return (
     <div className="w-full h-fit pt-16 flex flex-col items-start justify-start">
         <div className="w-full flex justify-between items-start mt-14">
-          <div className="flex flex-col gap-4 font-bold">
+          <div className="flex flex-col gap-4 font-bold items-start">
             <div className="flex flex-col gap-2">
               <h1 className="text-3xl">👋 Hello!</h1>
               <h1 className="text-3xl">I'm Afzal Khan</h1>
               <p className="font-medium">Passionate and creative full-stack software engineer from India 🇮🇳</p>
             </div>
-            <div className="flex items-center justify-around p-3 rounded-md">
-
+            <div className="flex items-center gap-6 justify-around">
+              <Link href={data.github} target="_blank"><GitHubLogoIcon className="w-[24px] h-[24px]"/></Link>
+              <Link href={data.linkedin} target="_blank"><LinkedInLogoIcon className="w-[24px] h-[24px]"/></Link>
+              <Link href={data.twitter} target="_blank"><TwitterLogoIcon className="w-[24px] h-[24px]"/></Link>
             </div>
           </div>
           <Image alt="img" width={150} height={150} src={'/static/profile-img.webp'} className="rounded-[50%]"/>
         </div>
+
+        {/**FEATURED PROJECTS */}
+        <section id="featured-projects" className="w-full h-fit mt-20">
+          <div className="flex items-center justify-between mb-8">
+            <h1 className="text-2xl font-bold gradient-one">Featured Projects</h1>
+            <div>
+              <Button variant='link' className="flex items-center justify-center gap-1 hover:gap-2 ease-in-out transition-all"><Link href='/projects'>View All</Link><ArrowRightIcon /></Button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-8">
+            {data.featuredProjects.map((project: any, index: number) => { return <FeaturedProject key={index} demoUrl={project.demoUrl} description={project.description} githubUrl={project.githubUrl} heading={project.heading} subHeading={project.subHeading} tech={project.tech} video={project.video} /> })}
+          </div>
+        </section>
     </div>
   );
-}
+} 
---
+++ src/components/ui/FeaturedProject.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { GitHubLogoIcon, GlobeIcon } from "@radix-ui/react-icons";
+
+
+interface FeaturedProjectI {
+    demoUrl: string,
+    githubUrl: string,
+    subHeading: string,
+    heading: string,
+    description: string,
+    tech: string[],
+    video: string
+}
+
+const FeaturedProject: React.FC<FeaturedProjectI> = (props) => {
+    return (
+        <div className='flex flex-col w-[100%] items-start'>
+      <span className='text-[#a29a59]'>{props.subHeading}</span>
+      <h1 className='font-bold text-xl'>{props.heading}</h1>
+      <div className={`mt-2 w-[100%] h-[32rem] flex flex-col boxshadow-one px-3 rounded-md gap-4 items-center justify-center`}>
+        <div className='w-[90%] mx-auto h-[20rem]'>
+          <video autoPlay src={props.video} className='w-[100%] h-[100%]' />
+        </div>
+        <span>{props.description}</span>
+        <div className='w-[100%] flex items-center justify-around'>
+          <Link href={props.githubUrl} target='_blank' className='w-[45%] py-1 flex items-center justify-center'>
+            <div className={`w-[100%] py-1 rounded-[.8rem] cursor-pointer flex items-center justify-center gap-2 border`}>
+              <GitHubLogoIcon />
+              <span>GitHub</span>
+            </div>
+          </Link>
+
+          <Link href={props.demoUrl} target='_blank' className='w-[45%] py-1 flex items-center justify-center'>
+            <div className={`w-[100%] py-1 rounded-[.8rem] cursor-pointer flex items-center justify-center gap-2 border`}>
+              <GlobeIcon />
+              <span>Demo</span>
+            </div>
+          </Link>
+        </div>
+        <div className='w-[100%] flex items-start justify-center gap-3 mt-2'>
+          {props.tech.map((t) => { return <span className='text-[#a29a59]'>{t}</span> })}
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default FeaturedProject;
