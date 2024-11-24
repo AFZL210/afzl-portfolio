07b2c92dd1de93ac48a7179812798cba0ea1d1bd
AFZL210 <afzal@zenskar.com>
2024-11-24 14:19:08 +0530

    fix padding

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className='w-[95vw] md:w-[900px] flex justify-between items-center h-16 fixed bg-bgprimary'>
       <Button variant='link' className="px-0"><Link href={'/'} className="font-bold">Afzal Khan</Link></Button>
       <div className="flex items-center justify-center gap-8">
-        <Button variant='link' className="px-0"><Link href={data.resume}>Resume</Link></Button>
+        <Button variant='link' className="px-0 mr-14 md:mr-10"><Link href={data.resume}>Resume</Link></Button>
         {/* <Button variant='link' className="px-0"><Link href='/blog'>Blog</Link></Button> */}
       </div>
     </div>
